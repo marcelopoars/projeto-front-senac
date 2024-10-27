8a1dfafe450a64e49b03fb92e04702cdd98d5b23
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-10-27 20:56:51 -0300

    feat: add Header and Footer components

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className=" bg-slate-100 text-sm text-center p-6">
+      <p>2024 | Projeto Gestão Ágil - UniSenac</p>
+      <p>[Nome do APP] Versão 0.0.1</p>
+      <p>Atualizado em 21/10/2024.</p>
+    </footer>
+  );
+}
