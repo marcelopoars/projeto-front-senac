d4d59a4bf3acbfd819535f817ebdd055d39d609c
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-10-28 11:07:17 -0300

    refactor: change Header and Footer styles

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="bg-slate-300">
-      <div className="container flex justify-between p-6">
-        <h1>LOGO do App</h1>
+      <div className="container flex items-center justify-between py-4 px-6">
+        <h1 className="font-bold">LOGO do App</h1>
 
         <nav className="flex gap-4">
-          <Link href="/login" className="rounded-lg p-2">
+          <Link href="/login" className="rounded-lg py-2 px-4">
             Entrar
           </Link>
-          <Link href="/login" className="bg-gray-600 rounded-lg text-white p-2">
+          <Link href="/login" className="bg-gray-600 rounded-lg text-white py-2 px-4">
             Cadastrar
           </Link>
         </nav>
