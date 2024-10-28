a31e56cabd01532dfa36580012be171f85df1759
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-10-28 14:41:25 -0300

    feat: apply color pattern to Header and Footer components

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -2,15 +2,20 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="bg-slate-300">
+    <header className="bg-sky-500">
       <div className="container flex items-center justify-between py-4 px-6">
-        <h1 className="font-bold">LOGO do App</h1>
+        <h1 className="font-bold text-white text-2xl">
+          <span className="text-3xl">🗓️</span> Agendar
+        </h1>
 
         <nav className="flex gap-4">
           <Link href="/login" className="rounded-lg py-2 px-4">
             Entrar
           </Link>
-          <Link href="/login" className="bg-gray-600 rounded-lg text-white py-2 px-4">
+          <Link
+            href="/login"
+            className="bg-sky-800 rounded-lg text-white py-2 px-4"
+          >
             Cadastrar
           </Link>
         </nav>
