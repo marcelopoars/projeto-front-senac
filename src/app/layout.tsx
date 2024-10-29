98acb4a6311c6b1830ce4c45b664894f3f4ebeac
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-10-29 12:09:03 -0300

    chore: test branch protection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     template: "Agendar | %s",
     default: "Agendar"
   },
-  description: "Encontre um profisional e agende um serviço. 123456789",
+  description: "Encontre um profisional e agende um serviço.",
 };
 
 export default function RootLayout({
