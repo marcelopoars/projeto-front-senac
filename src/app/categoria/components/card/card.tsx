3ee85d8f9c926a15a035507692ca2a8deabfde9d
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-10-29 23:42:11 -0300

    feat: inserindo page de categoria e cards, inii da estilização

---
+++ src/app/categoria/components/card/card.tsx
@@ -0,0 +1,14 @@
+export default function Card() {
+    return (
+      <div className="">
+        <img 
+            className=""
+            src="" 
+            alt="Card Image" 
+        />
+        <div className="">
+            <h2 className="">Categoria 1</h2>
+        </div>
+      </div>
+    );
+  }
