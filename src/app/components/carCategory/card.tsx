1ddaa00d6d47eec0a3035bd4265db555f6588278
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-10-30 23:09:23 -0300

    feat: acrescentando a categoria na page home

---
+++ src/app/components/carCategory/card.tsx
@@ -0,0 +1,12 @@
+export default function Card() {
+    return (
+      <div className="w-[300px] h-[200px] rounded overflow-hidden shadow-lg bg-white p-0 m-4">
+        <div className="h-[160px] w-full bg-gray-400">
+
+        </div>
+        <div className="w-full bottom-4 left-4 bg-white bg-opacity-75 px-2 py-1 rounded text-center">
+            <a href="http://" className="break-words">Categoria</a>
+        </div>
+      </div>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+
+import Card from "./components/carCategory/card";
+
 export default function Home() {
-  return <h2>HOME</h2>;
+  return (
+    <section className="w-[98%] m-4 p-6 w-1/2 h-90 flex flex-col justify-center items-center">
+    <div className="w-[50%] h-10 text-lg font-bold mb-6">
+        <h2 className="text-center h-14 font-bold text-2xl">
+            Qual serviço você deseja utilizar?
+        </h2>
+      </div>
+      <div className="w-[96%] flex flex-wrap justify-center">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </section>
+  )
 }
