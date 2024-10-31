34abccc7b5dac3bee1195eefb23a556529a7b384
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2024-10-30 23:28:39 -0300

    feat: acrescentando a categoria na page home

--- src/app/components/carCategory/card.tsx
+++ src/app/components/carCategory/card.tsx
@@ -1,11 +1,11 @@
 export default function Card() {
     return (
-      <div className="w-[300px] h-[200px] rounded overflow-hidden shadow-lg bg-white p-0 m-4">
+      <div className="w-[300px] h-[200px] rounded overflow-hidden shadow-lg bg-white p-0 m-4 transform transition duration-300 hover:scale-105">
         <div className="h-[160px] w-full bg-gray-400">
 
         </div>
-        <div className="w-full bottom-4 left-4 bg-white bg-opacity-75 px-2 py-1 rounded text-center">
-            <a href="http://" className="break-words">Categoria</a>
+        <div className="w-full h-[40px] bottom-4 left-4 bg-white bg-opacity-75 rounded text-center">
+            <a href="http://" className="block w-full h-full flex justify-center items-center break-words text-black-700 hover:text-[#0ea5e9]">Categoria</a>
         </div>
       </div>
     );
