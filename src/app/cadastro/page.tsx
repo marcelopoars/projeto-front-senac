2d5290281510bf254fa68b0815bb3f253b93fd95
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-11-05 22:58:52 -0300

    feat: add sing up form validations

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,11 +1,11 @@
-import { SignUpForm } from "./components";
+import { FormSignUp } from "./components";
 
 export default function SignUpPage() {
   return (
     <section>
       <div className="container px-6 py-12">
         <h1 className="font-bold text-3xl mb-8">Cadastro de Prestador</h1>
-        <SignUpForm />
+        <FormSignUp />
       </div>
     </section>
   );
