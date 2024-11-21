5c58743f989fb781199c90e23526ec3ad33dc517
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-11-21 19:50:53 -0300

    test: CI/CD test

--- src/utils/mask/normalize-cpf-or-cnpj.spec.ts
+++ src/utils/mask/normalize-cpf-or-cnpj.spec.ts
@@ -2,14 +2,14 @@ import { normalizeCpfOrCnpj } from "./normalize-cpf-or-cnpj";
 
 describe(":: Utils :: normalizeCpfOrCnpj", () => {
   it("should format a valid CPF", () => {
-    expect(normalizeCpfOrCnpj("12345678901")).toBe("123.456.789-01");
+    expect(normalizeCpfOrCnpj("12345678901")).toBe("123.456.789-01####");
   });
 
   it("should format a valid CNPJ", () => {
-    expect(normalizeCpfOrCnpj("12345678000195")).toBe("12.345.678/0001-95");
+    expect(normalizeCpfOrCnpj("12345678000195")).toBe("12.345.678/0001-95####");
   });
 
   it("should truncate extra digits for CNPJ", () => {
-    expect(normalizeCpfOrCnpj("123456780001950000")).toBe("12.345.678/0001-95");
+    expect(normalizeCpfOrCnpj("123456780001950000")).toBe("12.345.678/0001-95####");
   });
 });
