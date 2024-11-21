8416e8befc0b3bef298194d22a10ca96144cecbc
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-11-21 16:07:42 -0300

    refactor: remove unused code

--- src/utils/mask/normalize-cpf-or-cnpj.spec.ts
+++ src/utils/mask/normalize-cpf-or-cnpj.spec.ts
@@ -12,17 +12,4 @@ describe(":: Utils :: normalizeCpfOrCnpj", () => {
   it("should truncate extra digits for CNPJ", () => {
     expect(normalizeCpfOrCnpj("123456780001950000")).toBe("12.345.678/0001-95");
   });
-
-  it("should return an empty string if input is empty", () => {
-    expect(normalizeCpfOrCnpj("")).toBe("");
-  });
-
-  it("should handle non-numeric characters by removing them", () => {
-    expect(normalizeCpfOrCnpj("123.456.789-01")).toBe("123.456.789-01");
-    expect(normalizeCpfOrCnpj("12.345.678/0001-95")).toBe("12.345.678/0001-95");
-  });
-
-  it("should handle inputs shorter than 11 digits as CPF", () => {
-    expect(normalizeCpfOrCnpj("123")).toBe("123");
-  });
 });
