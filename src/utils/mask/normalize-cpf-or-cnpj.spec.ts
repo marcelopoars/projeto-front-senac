7cc221e8312d92c96ef57de9e62906b7c6086aa1
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-11-21 19:45:56 -0300

    chore: change CI/CD config

--- src/utils/mask/normalize-cpf-or-cnpj.spec.ts
+++ src/utils/mask/normalize-cpf-or-cnpj.spec.ts
@@ -2,7 +2,7 @@ import { normalizeCpfOrCnpj } from "./normalize-cpf-or-cnpj";
 
 describe(":: Utils :: normalizeCpfOrCnpj", () => {
   it("should format a valid CPF", () => {
-    expect(normalizeCpfOrCnpj("12345678901")).toBe("123.456.789/01");
+    expect(normalizeCpfOrCnpj("12345678901")).toBe("123.456.789-01");
   });
 
   it("should format a valid CNPJ", () => {
