b574ac734a7087af82b346d0aba8e3e3af10e62b
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-11-22 20:08:02 -0300

    refactor: change styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,15 +8,21 @@ export const metadata: Metadata = {
 
 export default function Error404() {
   return (
-    <div className="flex flex-col items-center justify-center align-center min-h-full f-column">
-      <h2 className="font-bold text-9xl text-sky-500 mb-4">404</h2>
-      <p className="text-3xl mb-4">Ops! Esta página não existe</p>
-      <Link
-        href={"/"}
-        className="text-sky-500 text-lg underline underline-offset-4"
-      >
-        Voltar para página inicial
-      </Link>
-    </div>
+    <section className="px-6 py-20 lg:py-32">
+      <div className="container text-center">
+        <h2 className="font-bold text-6xl text-sky-500 mb-4 lg:text-9xl">
+          404
+        </h2>
+
+        <p className="text-2xl mb-4 lg:text-3xl">Ops! Esta página não existe</p>
+
+        <Link
+          href={"/"}
+          className="text-sky-500 text-lg underline underline-offset-4"
+        >
+          Voltar para página inicial
+        </Link>
+      </div>
+    </section>
   );
 }
